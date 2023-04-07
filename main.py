350a8e818ca28f2718451fcb1f0cbc86c9a7b305
hsyoon <hsyoon94@snu.ac.kr>
2023-04-07 16:35:05 +0900

    hoho2

--- main.py
+++ main.py
@@ -45,177 +45,76 @@ def main():
     scheduler = optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.9999 ** epoch, last_epoch=-1, verbose=False)
 
     if args.experiment == 'husky':
-        # Get full husky dataset 
-        full_husky_dataset = raw_data_parser(args)
-        eval_dataset_coreset = eval_data_parser(args)
+        # Get full husky dataset
+        train_dataset = raw_data_parser(args)
+        eval_dataset = eval_data_parser(args)
         inference_dataset = inference_data_parser(args)
-        if args.offline_learning is False:
-            # Coreset Initialization
-            coreset = DatasetBuffer(args.coreset_buffer_size, args.coreset_type, args.c_r_class_num, args.experiment, "train")
-            for i in range(args.coreset_buffer_size):
-                coreset.append(full_husky_dataset[i][0], full_husky_dataset[i][1], None, 0)
-            
-            full_data_index = args.coreset_buffer_size
-
-            for cycle_idx in range(args.training_cycle):
-                
-                # Update coreset
-                new_data_ipt = full_husky_dataset[full_data_index][0]
-                new_data_opt = full_husky_dataset[full_data_index][1]
-                new_data_ipt_tensor = torch.tensor(new_data_ipt)
-                new_data_opt_tensor = torch.tensor(new_data_opt)
-                
-                # Train and update NN
-                model.train()
-                train(args, model, coreset, new_data_ipt_tensor, new_data_opt_tensor, args.iteration, cycle_idx, loss_function, optimizer, scheduler, device)
-                
-                if new_data_ipt_tensor is not None:
-                    coreset.append(new_data_ipt, new_data_opt, model, args.network_ensemble_cycle)
-                print("===============================================================================================================")
-                print("Exp time:", str(now_date[2:]) + "_" + str(now_time))
-                print("Corseet updated with cycle", cycle_idx+1, "/", args.training_cycle, ". Class", torch.argmax(new_data_opt_tensor).item(), "appended")
-                print()
-                for class_idx in range(args.c_r_class_num):
-                    print(" Class" + str(class_idx), end="    ")
-                print()
-                for class_idx in range(args.c_r_class_num):
-                    print("  ", len(coreset.c_n_classwise[class_idx]), end="      ")
-                print("\n")
-
-                full_data_index = full_data_index + 1
-                
-                torch.save(model, os.path.join(args.model_save_dir, "model.pt"))
-                # eval_acc = evaluation(args, cycle_idx, model, eval_dataset_coreset, loss_function, device)
-                eval_acc = evaluation(args, cycle_idx, model, full_husky_dataset, loss_function, device)
-                evaluation_accuracy_history.append(eval_acc)
-                print("===============================================================================================================\n\n")
-                # inference(args, cycle_idx, model, inference_dataset)
-
-        elif args.offline_learning is True:
-            for cycle_idx in range(args.training_cycle):
-                model.train()
-                train_offline(args, model, full_husky_dataset, loss_function, optimizer, scheduler, device)
-                eval_acc = evaluation(args, cycle_idx, model, eval_dataset_coreset, loss_function, device)
-                evaluation_accuracy_history.append(eval_acc)
 
     elif args.experiment == "mnist":
-        train_dataset = datasets.MNIST('dataparser/dataset/',train = True,download = True,
-                            transform = transforms.Compose([
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5), (0.5))
-                            ]))
-
-        eval_dataset = datasets.MNIST('dataparser/dataset/',train = False,download = True,
-                            transform = transforms.Compose([
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5), (0.5))
-                            ]))
-
-        for cycle_idx in range(args.training_cycle):
-                model.train()
-                train_offline(args, model, train_dataset, loss_function, optimizer, scheduler, device)
-                eval_acc = evaluation(args, 0, model, eval_dataset, loss_function, device)
-
-
-
-
-
+        train_dataset = datasets.MNIST('dataparser/dataset/',train = True,download = True,transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5), (0.5))]))
+        eval_dataset = datasets.MNIST('dataparser/dataset/',train = False,download = True,transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5), (0.5))]))
 
     elif args.experiment == "cifar10":
-        train_dataset = datasets.CIFAR10('dataparser/dataset/',train = True,download = True,
-                                transform = transforms.Compose([
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                                ]))
+        train_dataset = datasets.CIFAR10('dataparser/dataset/',train = True,download = True,transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+        eval_dataset = datasets.CIFAR10('dataparser/dataset/',train = False,download = True,transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
 
-        eval_dataset = datasets.CIFAR10('dataparser/dataset/',train = False,download = True,
-                                transform = transforms.Compose([
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                                ]))
-
-        if args.offline_learning is True:
-            for cycle_idx in range(args.training_cycle):
-                    model.train()
-                    train_offline(args, model, train_dataset, loss_function, optimizer, scheduler, device)
-                    eval_acc = evaluation(args, 0, model, eval_dataset, loss_function, device)
-        
-        elif args.offline_learning is False:
-            # Coreset Initialization
-            coreset = DatasetBuffer(args.coreset_buffer_size, args.coreset_type, args.c_r_class_num, args.experiment, "train")
-            
-            rnd_idx = random.sample(range(len(train_dataset)), len(train_dataset))
-            for i in range(args.coreset_buffer_size):
-                coreset.append(train_dataset[rnd_idx[i]][0], train_dataset[rnd_idx[i]][1], None, 0)
-            
-            full_data_index = args.coreset_buffer_size
-
-            for cycle_idx in range(args.training_cycle):
-                
-                # Update coreset
-                new_data_ipt = train_dataset[rnd_idx[full_data_index]][0]
-                new_data_opt = train_dataset[rnd_idx[full_data_index]][1]
-
-                if torch.is_tensor(new_data_ipt) is False:
-                    new_data_ipt_tensor = torch.tensor(new_data_ipt)
-                    new_data_opt_tensor = torch.tensor(new_data_opt)
-                else:
-                    new_data_ipt_tensor = new_data_ipt.clone().detach()
-                    new_data_opt_tensor = torch.tensor(new_data_opt).clone().detach()
-
-                # Train and update NN
-                model.train()
-                train(args, model, coreset, new_data_ipt_tensor, new_data_opt_tensor, args.iteration, cycle_idx, loss_function, optimizer, scheduler, device)
-                
-                if new_data_ipt_tensor is not None:
-                    coreset.append(new_data_ipt, new_data_opt, model, args.network_ensemble_cycle)
-                print("===============================================================================================================")
-                print("Exp time:", str(now_date[2:]) + "_" + str(now_time))
-                print("Corseet updated with cycle", cycle_idx+1, "/", args.training_cycle, ". Class", new_data_opt_tensor.item(), "appended")
-                print()
-                for class_idx in range(args.c_r_class_num):
-                    print(" Class" + str(class_idx), end="    ")
-                print()
-                for class_idx in range(args.c_r_class_num):
-                    print("  ", len(coreset.c_n_classwise[class_idx]), end="      ")
-                print("\n")
-
-                full_data_index = full_data_index + 1
-                
-                torch.save(model, os.path.join(args.model_save_dir, "model.pt"))
-                # eval_acc = evaluation(args, cycle_idx, model, eval_dataset_coreset, loss_function, device)
-                eval_acc = evaluation(args, cycle_idx, model, eval_dataset, loss_function, device)
-                evaluation_accuracy_history.append(eval_acc)
-                print("===============================================================================================================\n\n")
-                # inference(args, cycle_idx, model, inference_dataset)
-
-
-
-
-
-
-
-
-
-
-    
     elif args.experiment == "cifar100":
-        train_dataset = datasets.CIFAR100('dataparser/dataset/',train = True,download = True,transform = transforms.Compose([
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                            ]))
+        train_dataset = datasets.CIFAR100('dataparser/dataset/',train = True,download = True,transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+        eval_dataset = datasets.CIFAR100('dataparser/dataset/',train = False,download = True,transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
 
-        eval_dataset = datasets.CIFAR100('dataparser/dataset/',train = False,download = True,
-                            transform = transforms.Compose([
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                            ]))
+    if args.offline_learning is True:
+        for cycle_idx in range(args.training_cycle):
+            model.train()
+            train_offline(args, model, train_dataset, loss_function, optimizer, scheduler, device)
+            eval_acc = evaluation(args, 0, model, eval_dataset, loss_function, device)
+            evaluation_accuracy_history.append(eval_acc)
+
+    elif args.offline_learning is False:
+        coreset = DatasetBuffer(args.coreset_buffer_size, args.coreset_type, args.c_r_class_num, args.experiment, "train")
+        rnd_idx = random.sample(range(len(train_dataset)), len(train_dataset))
+        for i in range(args.coreset_buffer_size):
+            coreset.append(train_dataset[rnd_idx[i]][0], train_dataset[rnd_idx[i]][1], None, 0)
+        
+        full_data_index = args.coreset_buffer_size
 
         for cycle_idx in range(args.training_cycle):
-                model.train()
-                train_offline(args, model, train_dataset, loss_function, optimizer, scheduler, device)
-                eval_acc = evaluation(args, 0, model, eval_dataset, loss_function, device)
-    
+            
+            # Update coreset
+            new_data_ipt = train_dataset[rnd_idx[full_data_index]][0]
+            new_data_opt = train_dataset[rnd_idx[full_data_index]][1]
+
+            if torch.is_tensor(new_data_ipt) is False:
+                new_data_ipt_tensor = torch.tensor(new_data_ipt)
+                new_data_opt_tensor = torch.tensor(new_data_opt)
+            else:
+                new_data_ipt_tensor = new_data_ipt.clone().detach()
+                new_data_opt_tensor = torch.tensor(new_data_opt).clone().detach()
+
+            # Train and update NN
+            model.train()
+            train(args, model, coreset, new_data_ipt_tensor, new_data_opt_tensor, args.iteration, cycle_idx, loss_function, optimizer, scheduler, device)
+            
+            if new_data_ipt_tensor is not None:
+                coreset.append(new_data_ipt, new_data_opt, model, args.network_ensemble_cycle)
+            print("===============================================================================================================")
+            print("Exp time:", str(now_date[2:]) + "_" + str(now_time))
+            print("Corseet updated with cycle", cycle_idx+1, "/", args.training_cycle, ". Class", new_data_opt_tensor.item(), "appended")
+            print()
+            for class_idx in range(args.c_r_class_num):
+                print(" Class" + str(class_idx), end="    ")
+            print()
+            for class_idx in range(args.c_r_class_num):
+                print("  ", len(coreset.c_n_classwise[class_idx]), end="      ")
+            print("\n")
+
+            full_data_index = full_data_index + 1
+            
+            torch.save(model, os.path.join(args.model_save_dir, "model.pt"))
+            eval_acc = evaluation(args, cycle_idx, model, eval_dataset, loss_function, device)
+            evaluation_accuracy_history.append(eval_acc)
+            print("===============================================================================================================\n\n")
+            # inference(args, cycle_idx, model, inference_dataset)
+
         
     print("========== Training Ends  ===========")
     evaluation_accuracy_history_np = np.array(evaluation_accuracy_history)
